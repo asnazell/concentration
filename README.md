71b211ba85a5f06e842edf42108be538f8c185fd
Assel Snazell <assel.snazell@gmail.com>
2020-02-25 15:23:46 +1100

    final style updates and README

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Concentration Memory Game
 
 Concentration, also known as Match Match, Match Up, Memory, Pelmanism, Shinkei-suijaku, Pexeso or simply Pairs, is a card game in which all of the cards are laid face down on a surface and two cards are flipped face up over each turn. The object of the game is to turn over pairs of matching cards.
+
+## Challenge
+
+Match cards in less time with less moves.
+
+## How to Play
+
+Open index.html in your browser.
+
+## Instructions
+
+- Click on a card
+- Keep revealing cards and working your memory to remember each unveiled card
+- Match cards properly with less moves and in faster time
+
+## How I built the Memory Game
+
+Built with HTML, CSS and JS.
+
+- created a deck of cards that shuffles when game is refreshed
+- created a counter to count the number of moves made by player and timer to know the duration of a play
+- added effects to cards when they match and are unmatched
+- create a pop-up modal when player wins game
--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,79 @@
-console.log("Up and running!");
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
+const cardsInPlay = [];
 
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-if (cardsInPlay.length === 2) {
+//check for match function
+function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   } else {
     alert("Sorry, try again.");
   }
 }
+
+//flip card function
+function flipCard() {
+  // These 2 lines make the card faces appear instead of the card backs.
+  var cardId = this.getAttribute("data-id");
+  this.setAttribute("src", cards[cardId].cardImage);
+  // Adds value to cardsInPlay array.
+  cardsInPlay.push(cards[cardId].rank);
+  // Logs card flipped, image location & suit.
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+  // Check for match AFTER value has been added to cardsInPlay array.
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+}
+
+//createBoard function that loops through cards array
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+}
+createBoard();
+
+// console.log("Up and running!");
+
+// var cards = ["queen", "queen", "king", "king"];
+// var cardsInPlay = [];
+// var cardOne = cards[0];
+// cardsInPlay.push(cardOne);
+// console.log("User flipped queen");
+// var cardTwo = cards[2];
+// cardsInPlay.push(cardTwo);
+// console.log("User flipped king");
+// if (cardsInPlay.length === 2) {
+//   if (cardsInPlay[0] === cardsInPlay[1]) {
+//     alert("You found a match!");
+//   } else {
+//     alert("Sorry, try again.");
+//   }
+// }
